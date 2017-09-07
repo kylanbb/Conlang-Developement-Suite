999f9ddf9b9c005582752fdc4cf0bde81e46c477
Schyrsivochter <andreas.kuebrich@kuebrich.de>
2017-09-07 22:23:10 +0200

    Initial implementation of the Launcher window

--- src/common/launcher.py
+++ src/common/launcher.py
@@ -2,6 +2,25 @@
 """A ‘launcher’ window that gives a simple choice between different sub-apps and maybe recently used files (like LibreOffice does if you start it directly)"""
 
 import common
+import wx
 
 class LauncherWin(common.CDSWin):
     title = "Conlang Development Suite"
+    def build(self):
+        # this is not at all pretty, but for testing, it will do.
+        phonologyButton = wx.Button(self.frame, label="Phonology")
+        dictionaryButton = wx.Button(self.frame, label="Dictionary")
+        ## other apps
+        
+        self.frame.Sizer = wx.BoxSizer(wx.VERTICAL)
+        self.frame.Sizer.Add(phonologyButton, wx.EXPAND)
+        self.frame.Sizer.Add(dictionaryButton, wx.EXPAND)
+        
+        def onButton(windowType):
+            # Return a button click event handler
+            def handler(event):
+                wx.GetApp().newWindow(windowType)
+                self.frame.Close()
+            return handler
+        self.frame.Bind(wx.EVT_BUTTON, onButton("phonology"), phonologyButton)
+        self.frame.Bind(wx.EVT_BUTTON, onButton("dictionary"), dictionaryButton)
