bd93d7a05934b51d0169b5b73f566ea11a505be8
Schyrsivochter <andreas.kuebrich@kuebrich.de>
2017-09-10 14:19:59 +0200

    Add an ‘Apply’ button to filtering

--- src/dictionary/filtering.py
+++ src/dictionary/filtering.py
@@ -8,11 +8,16 @@ class Filter:
         self.panel = wx.Panel(parent.frame)
     def build(self):
         "Build the GUI and everything belonging to it."
-        # text entry and filter options, powerful but easy to understand
-        # to the ‘Apply Filter’ button or whatever, bind parent.filter
-        self.filterText = wx.TextCtrl(self.panel, value="filter", style=wx.TE_PROCESS_ENTER)
         self.panel.Sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.panel.Sizer.Add(self.filterText, proportion=1, flag=wx.EXPAND)
+        
+        # text entry and filter options, powerful but easy to understand
+        self.filterText = wx.TextCtrl(self.panel, value="filter", style=wx.TE_PROCESS_ENTER)
+        self.applyButton = wx.Button(self.panel, label="Apply")
+        self.panel.Sizer.Add(self.filterText, proportion=1, flag=wx.ALL, border=5)
+        self.panel.Sizer.Add(self.applyButton, proportion=0, flag=wx.ALL, border=5)
+        self.panel.Bind(wx.EVT_TEXT_ENTER, self.parent.filter, self.filterText)
+        self.panel.Bind(wx.EVT_BUTTON, self.parent.filter, self.applyButton)
+        # add stuff for properly controlling the filter
 
 class FilterOptions:
     "Data class representing the state of the filter."
