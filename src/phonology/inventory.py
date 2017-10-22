815be2999d18bfc28e28fbd77900385b59afa1a1
Schyrsivochter <andreas.kuebrich@kuebrich.de>
2017-10-22 20:56:18 +0200

    Some boilerplate code and structure for phoneme inventory

--- src/phonology/inventory.py
+++ src/phonology/inventory.py
@@ -2,9 +2,28 @@
 """The phoneme inventory editor"""
 
 from . import main
+from . import consonantBank
 
 class InventoryView(main.PhonologyView):
     def build(self):
         # make a split screen with two consonant charts and two vowel trapeziums;
         # one on top for the phonology being edited, one at the bottom for complete IPA
         pass
+
+class ConsonantTable:
+    def __init__(self, parent, readOnly):
+        self.readOnly = readOnly
+        self.panel = wx.Panel(parent.panel)
+        self.parent = parent
+    
+    def build(self):
+        # GridBagSizer for all the boxes
+        # every phoneme box gets its own window
+        # a static text or panel or something? Not sure
+        self.panel.Sizer = wx.GridBagSizer(vgap=5, hgap=5)
+        
+        for box in consonantBank.consonants:
+            ...
+            for sound in box.getModels():
+                ...
+            ...
